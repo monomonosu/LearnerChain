52add96f7a8e03becbc4d184a0b6037711f940c7
monomonosu <pojinyan@yahoo.co.jp>
2023-05-07 21:23:44 +0900

    フォント変更用グローバルCSS適用。

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app'
-import 'modern-css-reset/dist/reset.min.css' 
+import 'modern-css-reset/dist/reset.min.css'
+import '../styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
