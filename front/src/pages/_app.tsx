1fab9ae401d2683f7095ca232cff197d3425f08e
monomonosu <pojinyan@yahoo.co.jp>
2023-05-14 22:21:50 +0900

    themeをカスタマイズできるように対応。

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,7 +1,13 @@
 import type { AppProps } from 'next/app'
 import 'modern-css-reset/dist/reset.min.css'
 import '../styles/globals.css'
+import { ThemeProvider } from '@mui/material'
+import theme from '../../theme/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
---
+++ front/theme/theme.ts
@@ -0,0 +1,16 @@
+import { createTheme } from '@mui/material'
+
+const theme = createTheme({
+  palette: {
+    customGrey: {
+      main: '#F0F2EE',
+    },
+    customGreen: {
+      main: '#128938',
+      light: '#4CAF50',
+      dark: '#005F00',
+    },
+  },
+})
+
+export default theme
