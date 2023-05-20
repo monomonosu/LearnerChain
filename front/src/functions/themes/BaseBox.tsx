2f0855d003b281d469b1e3055fd82d9b501cbbdb
monomonosu <pojinyan@yahoo.co.jp>
2023-05-20 19:32:57 +0900

    BaseBoxはデフォルトで幅768pxになるように変更。

--- front/src/functions/themes/BaseBox.tsx
+++ front/src/functions/themes/BaseBox.tsx
@@ -2,6 +2,8 @@ import { styled } from '@mui/material/styles'
 import { Box } from '@mui/material'
 
 export const BaseBox = styled(Box)`
+  max-width: 768px;
+  margin: auto;
   min-height: calc(100vh - 70px);
   &.-flex {
     display: flex;
