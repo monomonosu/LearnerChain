e4de049b61367788c71d4a442173b9ae605fd1ac
monomonosu <pojinyan@yahoo.co.jp>
2023-05-23 03:23:49 +0900

    セッション追加ボタン追加。ヘッダーロゴにリンク追加。

--- front/src/components/layouts/HeaderMain.tsx
+++ front/src/components/layouts/HeaderMain.tsx
@@ -1,6 +1,8 @@
 import styled from '@emotion/styled'
+import Link from 'next/link'
 import { IconButton } from '@mui/material'
 import SearchIcon from '@mui/icons-material/Search'
+import PostAddIcon from '@mui/icons-material/PostAdd'
 import Image from 'next/image'
 import Logo from '../../assets/icons/LeanerChainLogo.svg'
 
@@ -22,10 +24,17 @@ export default function HeaderMain() {
     <>
       <HeaderContainer>
         <HeaderInner>
-          <Image src={Logo} width={100} height={50} alt="logo" />
-          <IconButton>
-            <SearchIcon />
-          </IconButton>
+          <Link href="/">
+            <Image src={Logo} width={100} height={50} alt="logo" />
+          </Link>
+          <div>
+            <IconButton>
+              <PostAddIcon fontSize="large" />
+            </IconButton>
+            <IconButton>
+              <SearchIcon fontSize="large" />
+            </IconButton>
+          </div>
         </HeaderInner>
       </HeaderContainer>
     </>
