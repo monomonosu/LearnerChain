21cc417395353588c039503c2a65983f601cd6cb
monomonosu <pojinyan@yahoo.co.jp>
2023-06-08 03:56:25 +0900

    モーダルコンポーネント作成。ヘッダーの検索アイコンに適用。

--- front/src/components/layouts/HeaderMain.tsx
+++ front/src/components/layouts/HeaderMain.tsx
@@ -5,8 +5,15 @@ import SearchIcon from '@mui/icons-material/Search'
 import PostAddIcon from '@mui/icons-material/PostAdd'
 import Image from 'next/image'
 import Logo from '../../assets/icons/LeanerChainLogo.svg'
+import { useDisclosure } from '@/functions/hooks/useDisclosure'
+import { BasicModal } from '../uis/BasicModal'
+import { BasicInput } from '../uis/BasicInput'
+import { BasicButton } from '../uis/BasicButton'
 
 export default function HeaderMain() {
+  // モーダル開閉管理
+  const searchModal = useDisclosure()
+
   const HeaderContainer = styled.div`
     min-height: 70px;
     display: flex;
@@ -19,6 +26,20 @@ export default function HeaderMain() {
     justify-content: space-between;
     align-items: center;
   `
+
+  const SearchContainer = styled.div`
+    display: grid;
+    gap: 20px;
+  `
+
+  // TODO:別ディレクトリに切り出した方が良いかも
+  const SearchForm: JSX.Element = (
+    <SearchContainer>
+      <p>ユーザー名、タイトル、タグ、URL（仮）を入力して検索できます。</p>
+      <BasicInput name="search_form" label="検索ワード" placeholder="もくもく会、勉強会" />
+      <BasicButton text="検索" height="50px" className="-shadow" />
+    </SearchContainer>
+  )
 
   return (
     <>
@@ -33,9 +54,15 @@ export default function HeaderMain() {
                 <PostAddIcon fontSize="large" />
               </IconButton>
             </Link>
-            <IconButton>
+            <IconButton onClick={searchModal.open}>
               <SearchIcon fontSize="large" />
             </IconButton>
+            <BasicModal
+              title="セッションを探す"
+              open={searchModal.isOpen}
+              handleClose={searchModal.close}
+              content={SearchForm}
+            ></BasicModal>
           </div>
         </HeaderInner>
       </HeaderContainer>
